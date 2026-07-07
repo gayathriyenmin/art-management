1f6d35ca83a1b0a4c08a7886602eb2d7b29475e8
Gayathri <gayathri.yenmin@gmail.com>
2026-07-07 11:25:07 +0530

    space added

--- src/components/sections/Features/Features.tsx
+++ src/components/sections/Features/Features.tsx
@@ -5,7 +5,7 @@ import { ACHIEVEMENTS } from '../../../constants';
 
 export const Features: React.FC = () => {
   return (
-    <section className="pb-20 md:pb-28 bg-bg-base relative">
+    <section className="py-20 md:py-28 bg-bg-base relative">
       <div className="max-w-7xl mx-auto px-6 md:px-8">
         <SectionTitle
           badge="Why Choose Us"
